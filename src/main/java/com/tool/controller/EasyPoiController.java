4b44fc348d8f1dcc5fbcc710ab30983b6ab1068f
YeGuanWei <787139598@qq.com>
2019-09-05 10:28:52 +0800

    新增了easypoi的动态导出

--- src/main/java/com/tool/controller/EasyPoiController.java
+++ src/main/java/com/tool/controller/EasyPoiController.java
@@ -4,6 +4,8 @@ import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
 import cn.afterturn.easypoi.excel.entity.params.ExcelExportEntity;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.*;
@@ -18,9 +20,49 @@ import java.util.Map;
 public class EasyPoiController {
 
     public static void main(String[] args) throws Exception {
-        //trendsExport();
-        //templateExport();
-        trendsExport2();
+        trendsExport3();
+    }
+
+    /**
+     * 动态导出3
+     * */
+    public static void trendsExport3() {
+        try {
+            List<ExcelExportEntity> entity = new ArrayList<ExcelExportEntity>();
+
+            entity.add(new ExcelExportEntity("姓名", "name"));
+            entity.add(new ExcelExportEntity("性别", "sex"));
+
+            List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+            for (int i = 1; i < 6; i++) {
+                Map map = new HashMap();
+
+                map.put("name", "姓名 " + i);
+                map.put("sex", "性别 " + i);
+
+                list.add(map);
+            }
+
+            // 把我们构造好的bean对象放到params就可以了
+            Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams(null, "t"), entity, list);
+
+            Sheet sheet = workbook.getSheet("t");
+
+            int i = 1;
+
+            Row row = sheet.createRow((short)(sheet.getLastRowNum()+1));
+            row.createCell(i++).setCellValue("546546");
+            row.createCell(i++).setCellValue("546546");
+            row.createCell(i++).setCellValue("546546");
+
+            FileOutputStream fos = new FileOutputStream("C:/excel/ExcelExportForMap.xls");
+            workbook.write(fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -38,6 +80,7 @@ public class EasyPoiController {
             List<ExcelExportEntity> temp = new ArrayList<ExcelExportEntity>();
             temp.add(new ExcelExportEntity("合并1", "students1"));
             temp.add(new ExcelExportEntity("合并2", "students2"));
+            temp.add(new ExcelExportEntity("合并3", "students3"));
             excelEntity.setList(temp);
 
             entity.add(excelEntity);
@@ -45,15 +88,18 @@ public class EasyPoiController {
             List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
             for (int i = 1; i < 6; i++) {
                 Map map = new HashMap();
-                map.put("name", "姓名 " + i);
-                map.put("sex", "性别 " + i);
 
                 List<Map> listMap = new ArrayList<>();
                 Map map1 = new HashMap();
-                map1.put("students1","合并1" + i);
+                map1.put("students2","合并2" + i);
+                map1.put("students3","合并3" + i);
                 listMap.add(map1);
 
                 map.put("students", listMap);
+
+
+                map.put("name", "姓名 " + i);
+                map.put("sex", "性别 " + i);
 
                 list.add(map);
             }
