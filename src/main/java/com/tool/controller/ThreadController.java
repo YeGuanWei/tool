41c714cd62352a64fd2c7a2d62d4c24d6f834bd7
YeGuanWei <787139598@qq.com>
2019-11-25 14:34:44 +0800

    1. 新增线程测试

--- src/main/java/com/tool/controller/ThreadController.java
+++ src/main/java/com/tool/controller/ThreadController.java
@@ -10,43 +10,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/thread")
 public class ThreadController {
 
-    private static long time = 5 * 1000;
+    private static long time = 60 * 1000;
 
     public static void main(String[] args) throws Exception {
         // normal();
         // sleep();
-        testWait();
+        // testWait();
     }
 
     @RequestMapping("/normal")
-    public static void normal() throws Exception {
-        throw new Exception("错误");
+    public static String normal() throws Exception {
+        System.out.println("收到Normal请求!");
+        if (true) {
+            throw new Exception("错误 - Normal");
+        }
+        return "normal response";
     }
 
     @RequestMapping("/sleep")
-    public static void sleep() throws Exception {
+    public String sleep() throws Exception {
+        System.out.println("收到Sleep请求!");
         Thread.sleep(time);
-        throw new Exception("错误");
+        if (true) {
+            throw new Exception("错误 - Sleep");
+        }
+        return "sleep response";
     }
 
     @RequestMapping("/wait")
-    public static void testWait() {
+    public String testWait() {
+        System.out.println("收到Wait请求!");
         TestWait t = new TestWait();
         t.start();
+
+        return "wait response";
     }
 
 }
 
 class TestWait extends Thread {
 
-    private static long time = 5 * 1000;
+    private static long time = 60 * 1000;
 
     public synchronized void run() {
         try {
             // 等待之后立即释放当前锁，并且进入等待池中等待唤醒
             // 当等待池中的线程被唤醒后，再次执行此语句之后的语句
             this.wait(time);
-            throw new Exception("错误");
+            throw new Exception("错误 - Wait Thread");
         }catch (Exception e) {
             e.printStackTrace();
         }
