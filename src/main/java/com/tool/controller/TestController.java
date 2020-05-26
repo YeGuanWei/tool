2cba6b381d2c773fd6281509505cbb522f3d340a
YeGuanWei <787139598@qq.com>
2020-05-26 14:54:33 +0800

    新增UUID和雪花算法

--- src/main/java/com/tool/controller/TestController.java
+++ src/main/java/com/tool/controller/TestController.java
@@ -2,19 +2,9 @@ package com.tool.controller;
 
 public class TestController {
 
-    public static void main(String[] args) throws Exception {
-        String inputFile = "C:/usr/local/yth/file/test.docx";
-        String outputFile = "C:/usr/local/yth/file/outTest.docx";
-
-        //加载示例文档
-        Document document = new Document(inputFile);
-
-        //删除第二条批注
-        document.getComments().removeAt(1);
-
-        //保存文档
-        document.saveToFile(outputFile, FileFormat.Docx);
-
+    public static void main(String[] args) {
+        String ule = "/sue/xciuys/1832sSuis[你好].doc";
+        System.out.println(ule.substring(0, ule.indexOf("[")));
     }
 
 }
---
+++ src/main/java/com/tool/utils/UUIDUtil.java
@@ -0,0 +1,12 @@
+package com.tool.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+	public static String getUUID() {
+		UUID uuid = UUID.randomUUID();
+		return uuid.toString();
+	}
+
+}
