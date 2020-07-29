c0c586349cf8d4b0ec409b920fe0dad5a7b0e61f
YeGuanWei <787139598@qq.com>
2020-07-29 09:39:17 +0800

    新增几个日期格式转换

--- src/main/java/com/tool/controller/date/DateController.java
+++ src/main/java/com/tool/controller/date/DateController.java
@@ -11,12 +11,72 @@ import java.util.Date;
 public class DateController {
 
     public static void main(String[] args) throws Exception {
+        dataSubstring();
+    }
+
+    /**
+     * 字符串转日期
+     */
+    private static void string2Date() {
+        System.out.println("字符串转日期============");
+        // 日期字符串
+        String dateStr = "1999-1-1 12:12:12";
+        // 日期格式
         String pattern = "yyyy-MM-dd HH:mm:ss";
-        pattern = "yyyy-MM-dd";
         SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-        String date = sdf.format(new Date());
-        System.out.println(date.substring(0,4));
-        System.out.println(date.substring(5));
+        try {
+            // 转换
+            Date date = sdf.parse(dateStr);
+            System.out.println(date);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * 日期转字符串
+     */
+    private static void date2String() {
+        System.out.println("日期转字符串============");
+        // 获取当前日期
+        Date date = new Date();
+        // 日期格式
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        try {
+            // 转换
+            String dateStr = sdf.format(date);
+            System.out.println(dateStr);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * 日期截取
+     */
+    private static void dataSubstring() {
+        System.out.println("日期截取============");
+        // 获取当前日期
+        Date date = new Date();
+        // 日期格式
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+
+        try {
+            // 转换
+            String dateStr = sdf.format(date);
+
+            System.out.println("年===" + dateStr.substring(0,4));
+            System.out.println("月===" + dateStr.substring(5,7));
+            System.out.println("日===" + dateStr.substring(8,10));
+            System.out.println("时===" + dateStr.substring(12,14));
+            System.out.println("分===" + dateStr.substring(15,17));
+            System.out.println("秒===" + dateStr.substring(18,20));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
