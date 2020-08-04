e0e7f836d424c1510421fe6325e309dc603966cd
YeGuanWei <787139598@qq.com>
2020-08-04 15:01:25 +0800

    优化项目结构

--- src/main/java/com/tool/controller/TestController.java
+++ src/main/java/com/tool/controller/TestController.java
@@ -3,8 +3,7 @@ package com.tool.controller;
 public class TestController {
 
     public static void main(String[] args) {
-        String ule = "/sue/xciuys/1832sSuis[你好].doc";
-        System.out.println(ule.substring(0, ule.indexOf("[")));
+
     }
 
 }
