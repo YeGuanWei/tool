47514a7a563dd2ec3b2634fec1ddf6c309350f1e
yeguanwei <787138598@qq.com>
2020-10-31 10:56:41 +0800

    q

---
+++ src/main/java/com/tool/model/entity/easypoi/EasyPoiExcelDTO.java
@@ -0,0 +1,26 @@
+package com.tool.model.entity.easypoi;
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import lombok.Data;
+
+@Data
+public class EasyPoiExcelDTO {
+
+    public static final String SUBJECT = "学科";
+
+    @Excel(name = "班级名称", width = 25, orderNum = "1", mergeVertical = true, needMerge = true)
+    private String className;
+
+    @Excel(name = "学院名称", width = 25, orderNum = "2")
+    private String userName;
+
+    @Excel(name = "语文", width = 15, groupName = SUBJECT, orderNum = "3")
+    private String chinese;
+
+    @Excel(name = "数学", width = 15, groupName = SUBJECT, orderNum = "4")
+    private String mathematics;
+
+    @Excel(name = "英语", width = 15, groupName = SUBJECT, orderNum = "5")
+    private String english;
+
+}
